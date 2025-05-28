43fc397997d7e38397e9e2be5c9d466c07dbde62
jrincon00 <jonanderincon@gmail.com>
2025-05-28 21:30:20 +0200

    Fallo corregido

--- cuatico-frontend/tailwind.config.js
+++ cuatico-frontend/tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*/.{html,ts}",
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {
-
       colors: {
         primario: '#4b30fc',
         secundario: '#d2ccff',
+      },
     },
   },
   plugins: [],
-  }
 }
