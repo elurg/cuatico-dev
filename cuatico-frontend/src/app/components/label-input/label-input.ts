fc77cfcfb5b81a0b7fbe06adecba682d46491016
elurg <elu.rgr@gmail.com>
2025-05-29 14:01:23 +0200

    label-input componente reutilizable creado

---
+++ cuatico-frontend/src/app/components/label-input/label-input.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-label-input',
+  standalone: true,
+  templateUrl: './label-input.html',
+  styleUrls: ['./label-input.css']
+})
+export class LabelInput {
+  @Input() label = '';
+  @Input() id = '';
+  @Input() type = 'text';
+  @Input() placeholder = '';
+  @Input() required = false;
+
+  @Input() model: any = '';
+  @Output() modelChange = new EventEmitter<any>();
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.modelChange.emit(value);
+  }
+}
