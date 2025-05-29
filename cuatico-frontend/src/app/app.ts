b8fb7214ab92210acd7c14e497255e549141ae5e
arp-10 <alejandrarodriguezp97@gmail.com>
2025-05-29 10:50:00 +0200

    chore: Config Schematics añadido

--- cuatico-frontend/src/app/app.ts
+++ cuatico-frontend/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Tarjeta } from './components/tarjeta/tarjeta';
-
+import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Tarjeta],
+  imports: [RouterOutlet, Navbar, Tarjeta, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
