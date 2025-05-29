9c200e1fd0f7f5c6e63a2fd2de2da3dabcd7d3a1
arp-10 <alejandrarodriguezp97@gmail.com>
2025-05-29 10:25:42 +0200

    Componente tarjeta creado

--- cuatico-frontend/src/app/app.ts
+++ cuatico-frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Tarjeta } from './components/tarjeta/tarjeta';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Tarjeta],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
