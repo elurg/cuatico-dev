25de9efb38453e665f27ec49c7f8f0fd4b307cd8
jrincon00 <jonanderincon@example.com>
2025-05-30 08:43:10 +0200

    Modelo tarjetas creado y fallo tarjetas arreglado

--- cuatico-frontend/src/app/components/courses/courses.ts
+++ cuatico-frontend/src/app/components/courses/courses.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Navbar } from '../navbar/navbar'; 
-import { Sidebar } from '../sidebar/sidebar'; // Sidebar is not exported from navbar
-
+import { Navbar } from '../navbar/navbar';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-courses',
@@ -14,8 +13,29 @@ import { Sidebar } from '../sidebar/sidebar'; // Sidebar is not exported from na
 export class Courses {
   title = 'Lista de Cursos';
   courses = [
-    { nombre: 'Matemáticas', profesor: 'Juan Pérez' },
-    { nombre: 'Historia', profesor: 'Ana Gómez' },
-    { nombre: 'Ciencias', profesor: 'Luis Martínez' }
+    {
+      nombre: 'UX Research',
+      categoria: 'Programación',
+      descripcion: 'Queremos compartir contigo las metodologías y técnicas más utilizadas actualmente que nos ayudan a entender el comportamiento humano y poner el foco en las necesidades, comportamientos y expectativas de los usuarios.Aprenderás a realizar investigaciones efectivas que informen y mejoren el diseño de productos y servicios digitales.',
+      profesor: 'Diana Henao',
+      lecciones: 'XX',
+      duracion: '16h',
+    },
+    {
+      nombre: 'User Experience',
+      categoria: 'Diseño',
+      descripcion: 'Domina los principios de UX para diseñar productos digitales atractivos y funcionales.',
+      profesor: 'Ana Gómez',
+      lecciones: 18,
+      duracion: '7h 45m',
+    },
+    {
+      nombre: 'Análisis de Datos',
+      categoria: 'Datos',
+      descripcion: 'Descubre cómo analizar e interpretar datos para tomar mejores decisiones empresariales.',
+      profesor: 'Luis Martínez',
+      lecciones: 24,
+      duracion: '9h 10m',
+    }
   ];
 }
