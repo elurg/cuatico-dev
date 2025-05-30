d170bdf721f7e09ac2197c4a1836d82aa30eafb4
elurg <elurg@gmail.com>
2025-05-30 20:22:07 +0200

    Arreglo de la navbar

--- cuatico-frontend/src/app/core/navbar/navbar.ts
+++ cuatico-frontend/src/app/core/navbar/navbar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Searchbar } from "../searchbar/searchbar";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [Searchbar, RouterModule],
   templateUrl: './navbar.html'
 })
 export class Navbar {
