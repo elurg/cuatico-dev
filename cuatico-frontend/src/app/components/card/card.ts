ad5cf770028d34b368cef03459c353cc3e617bbe
arp-10 <alejandrarodriguezp97@gmail.com>
2025-05-30 10:16:21 +0200

    Componente Card mejorado. Ahora es reutilizable

--- cuatico-frontend/src/app/components/card/card.ts
+++ cuatico-frontend/src/app/components/card/card.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
-  templateUrl: './card.html'
+  templateUrl: './card.html',
+  standalone: true,
+  imports: [CommonModule]
 })
 export class Card {
-
+  @Input() titulo!: string;
+  @Input() descripcion?: string;
+  @Input() profesor?: string;
+  @Input() lecciones?: number | string;
+  @Input() duracion?: number | string;
+  @Input() precio?: number | string;
+  @Input() tipo!: 'venta' | 'curso' | 'certificado';
 }
--- cuatico-frontend/src/app/components/courses/courses.ts
+++ cuatico-frontend/src/app/components/courses/courses.ts
@@ -1,41 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Navbar } from '../navbar/navbar';
 import { Sidebar } from '../sidebar/sidebar';
+import { Card } from '../card/card';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.html',
-  styleUrls: ['./courses.css'],
-  standalone: true,
-  imports: [CommonModule, Navbar, Sidebar]
+  imports: [CommonModule, Navbar, Sidebar, Card]
 })
 export class Courses {
-  title = 'Lista de Cursos';
-  courses = [
-    {
-      nombre: 'UX Research',
-      categoria: 'Programación',
-      descripcion: 'Queremos compartir contigo las metodologías y técnicas más utilizadas actualmente que nos ayudan a entender el comportamiento humano y poner el foco en las necesidades, comportamientos y expectativas de los usuarios.Aprenderás a realizar investigaciones efectivas que informen y mejoren el diseño de productos y servicios digitales.',
-      profesor: 'Diana Henao',
-      lecciones: 'XX',
-      duracion: '16h',
-    },
-    {
-      nombre: 'User Experience',
-      categoria: 'Diseño',
-      descripcion: 'Domina los principios de UX para diseñar productos digitales atractivos y funcionales.',
-      profesor: 'Ana Gómez',
-      lecciones: 18,
-      duracion: '7h 45m',
-    },
-    {
-      nombre: 'Análisis de Datos',
-      categoria: 'Datos',
-      descripcion: 'Descubre cómo analizar e interpretar datos para tomar mejores decisiones empresariales.',
-      profesor: 'Luis Martínez',
-      lecciones: 24,
-      duracion: '9h 10m',
-    }
-  ];
 }
