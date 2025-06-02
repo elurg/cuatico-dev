54c878ccf1fe4f8aedbd741dfc390e1abbeffe42
jrincon00 <jonanderincon@gmail.com>
2025-06-02 20:30:16 +0200

    Component card arreglado y certificado funcionando a medias

--- cuatico-frontend/src/app/core/certificate/certificate.ts
+++ cuatico-frontend/src/app/core/certificate/certificate.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Card } from '../card/card';
 
 @Component({
   selector: 'app-certificate',
-  imports: [],
+  imports: [CommonModule, Card],
   templateUrl: './certificate.html'
 })
 export class Certificate {
+
+
 
 }
