397adae6752be2f69a6fccada7c6d1af7191b6d5
jrincon00 <jonanderincon@example.com>
2025-06-05 09:52:21 +0200

    Pruebas

---
+++ cuatico-frontend/src/app/components/pruebas/pruebas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pruebas',
+  imports: [],
+  templateUrl: './pruebas.html',
+  styleUrl: './pruebas.css'
+})
+export class Pruebas {
+
+}
